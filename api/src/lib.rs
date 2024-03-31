1f8b1bad1352b4ad057dd60ebac21aa6334591bf
000sub <dt2980_hee@naver.com>
2024-04-01 01:05:55 +0900

    Feat: scaffold template code

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::env;
+
+use axum::extract::Path;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::Router;
+use axum::routing::get;
+
+mod response;
+
+#[tokio::main]
+async fn start() -> anyhow::Result<()> {
+    env::set_var("RUST_LOG", "debug");
+    tracing_subscriber::fmt::init();
+
+    dotenvy::dotenv().ok();
+    let host = env::var("HOST").expect("HOST is not set in .env file");
+    let port = env::var("PORT").expect("PORT is not set in .env file");
+    let server_url = format!("{host}:{port}");
+
+    //TODO
+    let app = Router::new()
+        .route("/:address", get(find_token));
+
+    let listener = tokio::net::TcpListener::bind(&server_url).await.unwrap();
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+async fn find_token(
+    Path(address): Path<String>
+) -> impl IntoResponse {
+    (StatusCode::OK, format!("Token Address: {}", address))
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn main() {
+    let result = start();
+
+    if let Some(err) = result.err() {
+        println!("Error: {err}");
     }
 }
